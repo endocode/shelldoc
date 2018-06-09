32581fd5861d1299e6bfeed899ef40b6da7ec779
Mirko Boehm (Endocode) <mirko@endocode.com>
2018-06-09 11:04:56 +0200

    Lay out expected interaction with the interactions in main.

--- cmd/shelldoc/shelldoc.go
+++ cmd/shelldoc/shelldoc.go
@@ -1,38 +1,39 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/Endocode/shelldoc/pkg/shell"
-
-	"gopkg.in/russross/blackfriday.v2"
+	"github.com/Endocode/shelldoc/pkg/tokenizer"
 )
 
-func visitor(node *blackfriday.Node, entering bool) blackfriday.WalkStatus {
-	fmt.Printf("node: %s - %s - entering: %v.\n", node.Type, node.Literal, entering)
-	if node.Type == blackfriday.CodeBlock {
-		// fmt.Printf("node: %s - %s - entering: %v.\n", node.Type, node.Literal, entering)
-	}
-	return blackfriday.GoToNext
-}
+// func visitor(node *blackfriday.Node, entering bool) blackfriday.WalkStatus {
+// 	fmt.Printf("node: %s - %s - entering: %v.\n", node.Type, node.Literal, entering)
+// 	if node.Type == blackfriday.CodeBlock {
+// 		// fmt.Printf("node: %s - %s - entering: %v.\n", node.Type, node.Literal, entering)
+// 	}
+// 	return blackfriday.GoToNext
+// }
 
 func main() {
-	if data, err := ReadInput(os.Args[1:]); err != nil {
+	data, err := ReadInput(os.Args[1:])
+	if err != nil {
 		log.Fatalln(err)
-	} else {
-		shell, err := shell.StartShell()
-		if err != nil {
-			log.Fatalln(err)
-		}
-		defer shell.Exit()
-
-		md := blackfriday.New()
-		om := md.Parse(data)
-		om.Walk(visitor)
-		fmt.Println("--------------------")
-		fmt.Println(string(data))
-		fmt.Println("Done.")
+	}
+	// run the input through the tokenizer
+	visitor := tokenizer.NewInteractionVisitor()
+	tokenizer.Tokenize(data, visitor)
+	// start a background shell, it will run until the program ends
+	shell, err := shell.StartShell()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer shell.Exit()
+	// execute the interactions and verify the results:
+	for index, interaction := range visitor.Interactions {
+		log.Printf("[%2d]: %s\n", index, interaction.Cmd)
+		log.Printf("=--> : %s", interaction.Cmd)
+		log.Printf("<--= : PASS (%s)", "regex match")
 	}
 }
