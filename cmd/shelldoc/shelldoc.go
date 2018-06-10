434b11266d1d5d84731a924090c706cf9078dfc0
Mirko Boehm (Endocode) <mirko@endocode.com>
2018-06-10 12:06:20 +0200

    Handle command line flags, add logging configuration.

--- cmd/shelldoc/shelldoc.go
+++ cmd/shelldoc/shelldoc.go
@@ -1,18 +1,34 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"io/ioutil"
+	golog "log"
 	"os"
 
 	"github.com/Endocode/shelldoc/pkg/shell"
 	"github.com/Endocode/shelldoc/pkg/tokenizer"
+	"github.com/spf13/pflag"
 )
 
 const (
 	returnSuccess = iota
 	returnFailure
 	returnError
+)
+
+// Options contains the context of a program invocation
+type Options struct {
+	shell   string // The shell to invoke
+	verbose bool   // Enable trace log output
+}
+
+// global variables
+var (
+	options Options
+	// Debug receives log messages in verbose mode
+	Debug *golog.Logger
+	// Log is the standard logger
+	Log *golog.Logger
 )
 
 func max(a, b int) int {
@@ -34,9 +50,20 @@ func result(code int) string {
 }
 
 func main() {
-	data, err := ReadInput(os.Args[1:])
+	pflag.StringVarP(&options.shell, "shell", "s", "", "The shell to invoke (default: the user's shell).")
+	pflag.BoolVarP(&options.verbose, "verbose", "v", false, "Enable diagnostic log output.")
+	pflag.Parse()
+	if options.verbose {
+		Debug = golog.New(os.Stderr, "", 0)
+	} else {
+		Debug = golog.New(ioutil.Discard, "", 0)
+	}
+	Log = golog.New(os.Stderr, "", 0)
+	args := pflag.Args()
+
+	data, err := ReadInput(args)
 	if err != nil {
-		log.Fatalln(err)
+		Log.Fatalln(err)
 	}
 	// run the input through the tokenizer
 	visitor := tokenizer.NewInteractionVisitor()
@@ -44,7 +71,7 @@ func main() {
 	// start a background shell, it will run until the program ends
 	shell, err := shell.StartShell()
 	if err != nil {
-		log.Fatalln(err)
+		Log.Fatalln(err)
 	}
 	defer shell.Exit()
 	returncode := returnSuccess
@@ -55,14 +82,14 @@ func main() {
 	// execute the interactions and verify the results:
 	for index, interaction := range visitor.Interactions {
 		testCount++
-		fmt.Printf("COMMAND (%d): %s\n", index+1, interaction.Describe())
-		fmt.Printf("--> %s\n", interaction.Cmd)
+		Log.Printf("COMMAND (%d): %s\n", index+1, interaction.Describe())
+		Debug.Printf("--> %s\n", interaction.Cmd)
 		if err := interaction.Execute(&shell); err != nil {
-			fmt.Printf("--  ERROR: %v\n", err)
+			Log.Printf("--  ERROR: %v\n", err)
 			returncode = max(returncode, returnError)
 			errorCount++
 		}
-		fmt.Printf("<-- %s\n", interaction.Result())
+		Debug.Printf("<-- %s\n", interaction.Result())
 		if interaction.HasFailure() {
 			returncode = max(returncode, returnFailure)
 			failureCount++
@@ -70,6 +97,6 @@ func main() {
 			successCount++
 		}
 	}
-	fmt.Printf("%s: %d tests (%d successful, %d failures, %d execution errors)\n", result(returncode), testCount, successCount, failureCount, errorCount)
+	Log.Printf("%s: %d tests (%d successful, %d failures, %d execution errors)\n", result(returncode), testCount, successCount, failureCount, errorCount)
 	os.Exit(returncode)
 }
