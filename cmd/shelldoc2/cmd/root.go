616999027e4b9237419dae40710b0dafec039fa6
Mirko Boehm (Endocode) <mirko@endocode.com>
2019-05-09 18:43:52 +0200

    Finish the root command implementation.

--- cmd/shelldoc2/cmd/root.go
+++ cmd/shelldoc2/cmd/root.go
@@ -6,32 +6,27 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
-var cfgFile string
+var verbose bool
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "shelldoc2",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Use:   "shelldoc",
+	Short: "shelldoc tests Unix shell commands in Markdown documentation",
+	Long: `Markdown is widely used for documentation and README.md files that explain how
+to use or build some software. Such documentation often contains shell commands that
+explain how to build a software or how to run it. To make sure the documentation is a
+ccurate and up-to-date, it should be automatically tested. shelldoc tests Unix shell
+commands in Markdown files and reports the results.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -40,40 +35,17 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.shelldoc2.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	cobra.OnInitialize(initLogging)
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "enable diagnostics")
 }
 
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
+func initLogging() {
+	// verbose essentially enables or disables log output:
+	if verbose {
+		log.SetOutput(os.Stderr)
 	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".shelldoc2" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".shelldoc2")
+		log.SetOutput(ioutil.Discard)
 	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
+	log.SetFlags(0)
+	log.SetPrefix("Note: ")
 }
